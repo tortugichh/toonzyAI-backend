cc53f6806ec1a5c3b6250d1d0cdc79c3a0a22f80
tortugichh <otesh0621@gmail.com>
2025-06-24 22:53:49 +0500

    bucket fix

--- utils/gcs_client.py
+++ utils/gcs_client.py
@@ -9,6 +9,5 @@ def upload_image_to_gcs(image_bytes: bytes, filename: str) -> str:
     bucket = client.bucket(GCS_BUCKET)
     blob = bucket.blob(filename)
     blob.upload_from_string(image_bytes, content_type="image/png")
-    # Сделать объект публичным
-    blob.make_public()
+    # blob.make_public()  # Удалено для совместимости с uniform bucket-level access
     return blob.public_url 
